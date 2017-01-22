403db6fb91091afc693aaecc73684a7ee9b43320
juju4 <juju4@users.noreply.github.com>
2017-01-22 12:09:53 -0500

    switch travis to docker tests, aide path fix, add google santa for darwin macports upgrade, s/reboot/shutdown -r/

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,12 +9,7 @@ else
 rolesdir=$(dirname $0)/..
 fi
 
-[ ! -d $rolesdir/juju4.maxmind ] && git clone https://github.com/juju4/ansible-maxmind $rolesdir/juju4.maxmind
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
-[ ! -d $rolesdir/kbrebanov.selinux ] && git clone https://github.com/kbrebanov/ansible-selinux.git $rolesdir/kbrebanov.selinux
-[ ! -d $rolesdir/kbrebanov.dkms ] && git clone https://github.com/kbrebanov/ansible-dkms.git $rolesdir/kbrebanov.dkms
-[ ! -d $rolesdir/kbrebanov.kernel_headers ] && git clone https://github.com/kbrebanov/ansible-kernel_headers.git $rolesdir/kbrebanov.kernel_headers
-[ ! -d $rolesdir/kbrebanov.virtualbox ] && git clone https://github.com/kbrebanov/ansible-virtualbox.git $rolesdir/kbrebanov.virtualbox
+#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.upgrade-pkgs ] && ln -s ansible-upgrade-pkgs $rolesdir/juju4.upgrade-pkgs
 [ ! -e $rolesdir/juju4.upgrade-pkgs ] && cp -R $rolesdir/ansible-upgrade-pkgs $rolesdir/juju4.upgrade-pkgs
